20b8340a88b6d0433bf149ebd924ab79b1e10834
sosoYim <sohyung.yim@gmail.com>
2022-11-26 08:56:08 +0900

    fix: yarn start시 build 먼저 해줌

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       extends: 'lighthouse:default',
-      startServerCommand: 'yarn start', // 서버를 키는 명령어를 통해서도 실행 가능
+      startServerCommand: 'yarn start:build', // 서버를 키는 명령어를 통해서도 실행 가능
       url: ['http://localhost:3000'],
       numberOfRuns: 3,
     },
